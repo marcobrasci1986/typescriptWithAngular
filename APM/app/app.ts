d19f148058a1f54b72f743b15e26d1e3cead31db
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-02-23 10:51:13 +0100

    Adding a routes

--- APM/app/app.ts
+++ APM/app/app.ts
@@ -1,7 +1,23 @@
-/// <reference path="../../typings/angularjs/angular.d.ts" />
-
 module app {
-    angular.module('productManagement',
-        ['productResourceMock',
+    var main = angular.module('productManagement',
+        ['ngRoute',
+            'productResourceMock',
             'common.services']);
+
+    main.config(routeConfig);
+
+    routeConfig.$inject = ['$routeProvider'];
+    function routeConfig($routeProvider:ng.route.IRouteProvider):void {
+
+        $routeProvider
+            .when('/productList', {
+                templateUrl: 'app/products/productListView.html',
+                controller: 'ProductListController as vm'
+            })
+            .when('/productDetail/:productId', {
+                templateUrl: 'app/products/productDetailView.html',
+                controller: 'ProductDetailController as vm'
+            })
+            .otherwise('/productList');
+    }
 }
